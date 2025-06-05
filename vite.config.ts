b7e47abe9ab0dfaacc3bc8b4961edead69158bd9
Christoffer Pedersen <securebydesign@outlook.dk>
2025-06-05 15:02:00 +0200

    chore: update GitHub Actions to v3 and fix base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/secure-by-design-new/',
+  base: 'https://securebydesign.dk/',
   build: {
     outDir: 'dist',
     sourcemap: true,
